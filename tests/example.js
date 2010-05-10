9022b0563c43dff537f82129de2c4f5fc4c55548
heungsub <heungsub@lunant.net>
2010-05-10 11:48:01 +0900

    New test: `example.js`

---
+++ tests/example.js
@@ -0,0 +1,35 @@
+/**
+This is the "example" module.
+
+The example module supplies one function, factorial().  For example,
+
+> factorial( 5 );
+120
+*/
+
+function factorial( n ) {
+    /**
+    > factorial( 1 );
+    1
+    > factorial( 30 );
+    2.6525285981219103e+32
+    */
+
+    if ( n < 0 ) {
+        throw new Error( "n must be >= 0" );
+    } else if ( Math.floor( n ) !== n ) {
+        throw new Error( "n must be exact integer" );
+    } else if ( n + 1 === n ) {
+        throw new Error( "n too large" );
+    }
+
+    var result = 1, factor = 2;
+
+    while ( factor <= n ) {
+        result *= factor;
+        factor += 1;
+    }
+
+    return result;
+}
+

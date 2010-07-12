115ba216483f09a51a4e581f9d2ef4d9ca9c02a5
Heungsub Lee <lee@heungsub.net>
2010-07-12 09:47:08 +0900

    Changed my name to "Lee Heung-sub".

--- doctest/intro.js
+++ doctest/intro.js
@@ -1,9 +1,9 @@
 /*!
  * doctest.js (@VERSION)
- * http://lab.heungsub.net/doctest.js
+ * http://lab.sublee.kr/doctest.js
  * http://github.com/lunant/doctest.js
  * 
- * Copyright 2010, Heungsub Lee <heungsub+doctest@lunant.net>
+ * Copyright 2010, Lee Heung-sub <heungsub+doctest@lunant.net>
  * Dual licensed under the MIT or GPL Version 2 licenses.
  * See also MIT-LICENSE.txt and GPL-LICENSE.txt 
  *

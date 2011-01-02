fc18ea31c0a9f78999fca8211c3a7890204e8226
Heungsub Lee <heung@sublee.kr>
2011-01-02 23:42:56 +0900

    hypen -> en dash

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'jDoctest'
-copyright = u'2010-2011, Heungsub Lee'
+copyright = u'2010–2011, Heungsub Lee'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
